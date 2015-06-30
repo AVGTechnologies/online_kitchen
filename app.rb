29b53ca309807981b516bdc80d1acfcec385bb2d
petr.schreiber <petr.schreiber@avg.com>
2015-06-30 14:41:35 +0200

    Feature: Skeleton for REST routes

--- app.rb
+++ app.rb
@@ -1,24 +1,40 @@
 # Service providing the Online Kitchen functionality
 
 $: << 'lib'
+require 'sinatra/base'
 require 'bundler/setup'
 require 'online_kitchen'
-require 'sinatra/base'
 require 'active_support'
-
 
 module OnlineKitchen
   class App < Sinatra::Base
 
     set :bind, '0.0.0.0'
-    before do
-      auth
-    end
 
-    get '/templates' do
-      content_type :json
-      OnlineKitchenTemplate.all.to_json
-    end
+    # TODO: namespace '/api/v1/' do
+      get '/api/v1/templates' do
+        common_request_setup
+        OnlineKitchenTemplate.all.to_json
+      end
+
+      post '/api/v1/configurations' do
+        common_request_setup
+        @configuration_specification = get_body_as_json
+      end
+
+      get '/api/v1/configurations' do
+        common_request_setup
+      end
+
+      put '/api/v1/configurations' do
+        common_request_setup
+        @configuration_modification = get_body_as_json
+      end
+
+      delete '/api/v1/configurations/:configuration_id' do |configuration_id|
+        common_request_setup
+      end
+    #end
 
     private
 
@@ -32,6 +48,17 @@ module OnlineKitchen
       end
     end
 
-    run! if app_file == $0
+    def common_request_setup
+      auth
+      content_type :json
+      response['Access-Control-Allow-Origin'] = '*'
+    end
+
+    def get_body_as_json
+      request.body.rewind
+      JSON.parse request.body.read
+    end
+
+    run! if app_file == $0 # This makes the app launchanble like "ruby app.rb"
   end
 end
--- lib/models/online_kitchen_template.rb
+++ lib/models/online_kitchen_template.rb
@@ -2,6 +2,7 @@
 class OnlineKitchenTemplate < Settingslogic
   source "#{OnlineKitchen.root}/config/templates.yml"
   namespace OnlineKitchen.env
+  # TODO: suppres_errors OnlineKitchen.env.production?
   load!
 
   def all

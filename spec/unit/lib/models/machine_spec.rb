6b9b677eae2d0b673982328656f30eddbae73908
petr.schreiber <petr.schreiber@avg.com>
2015-07-23 15:27:39 +0200

    Fix: Adjusted unit tests to reflect best practices learned at code school

--- spec/unit/lib/models/machine_spec.rb
+++ spec/unit/lib/models/machine_spec.rb
@@ -2,30 +2,31 @@ require 'spec_helper'
 require 'sidekiq/testing'
 
 describe 'Machine' do
-  let(:machine) { Machine.new }
+  let!(:machine) { Machine.new }
+  subject { machine }
 
   it "sets deleted flag when appropriate state is set" do
-    machine.state = "deleted"
+    subject.state = "deleted"
     expect(machine.deleted?).to be true
   end
 
   it "sets destroy_queued flag when appropriate state is set" do
-    machine.state = "destroy_queued"
+    subject.state = "destroy_queued"
     expect(machine.destroy_queued?).to be true
   end
 
   it "sets deleted_or_destroy_queued flag when deleted state is set" do
-    machine.state = "deleted"
+    subject.state = "deleted"
     expect(machine.deleted_or_destroy_queued?).to be true
   end
 
   it "sets deleted_or_destroy_queued flag when deleted state is set" do
-    machine.state = "destroy_queued"
+    subject.state = "destroy_queued"
     expect(machine.deleted_or_destroy_queued?).to be true
   end
 
   it "sets state to destroy when schedule_destroy is called" do
-    machine.schedule_destroy
+    subject.schedule_destroy
     expect(machine.destroy_queued?).to be true
   end
 end

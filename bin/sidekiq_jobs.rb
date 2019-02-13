74227057aca1f5349a6a8d8dbb29f30dd14ed93f
Ivo Reznicek <ireznice@googlemail.com>
2019-02-13 06:50:11 +0100

    fix: sidekiq logging

--- bin/sidekiq_jobs.rb
+++ bin/sidekiq_jobs.rb
@@ -5,5 +5,5 @@ require 'online_kitchen'
 
 OnlineKitchen.setup
 
-# Sidekiq::Logging.logger = OnlineKitchen.logger
-# Sidekiq::Logging.logger.level = OnlineKitchen.logger.level
+Sidekiq::Logging.logger = OnlineKitchen.logger
+Sidekiq::Logging.logger.level = OnlineKitchen.config.log_level
